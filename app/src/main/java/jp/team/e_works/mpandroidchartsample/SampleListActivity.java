0696bac2d271756e2f18d0d7e2cab84eaeacd651
kei <entan.518@gmail.com>
2018-03-01 23:57:08 +0900

    Initial commit

---
+++ app/src/main/java/jp/team/e_works/mpandroidchartsample/SampleListActivity.java
@@ -0,0 +1,13 @@
+package jp.team.e_works.mpandroidchartsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SampleListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_list);
+    }
+}
